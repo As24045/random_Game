d682f294fdfc60753279da11be9c1a4a1008c47e
Amit Kumar Sharma <138234619+As24045@users.noreply.github.com>
2023-10-08 01:21:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,104 @@
+let randomNumber = parseInt(Math.random()*100 +1);
+
+const submit = document.querySelector('#subt');
+const userInput = document.querySelector('#guessfield');
+const guessSlot = document.querySelector('.guesses');
+const remaining = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+const startOver = document.querySelector('.resultParas');
+
+const p = document.createElement('p');
+
+let prevGuess = [];
+let numGuess = 1;
+
+let playGame = true;
+
+if (playGame) {
+    submit.addEventListener('click', function (event) {
+        event.preventDefault();
+        const guess = parseInt(userInput.value);
+        //console.log(guess)
+        validateGuess(guess);
+    })
+}
+
+
+function validateGuess(guess) {
+    if (isNaN(guess)) {
+        alert('please Inter the valid number');
+    }
+    else if(guess < 1){
+        alert('please Inter more then 1');
+    }
+    else if(guess > 100){
+        alert('please Inter less 100');
+    }
+    else {
+        prevGuess.push(guess);
+
+        if (numGuess ===10 ) {
+            displayGuess(guess);
+            displayMassage(`Game Over. Random number was ${randomNumber}`);
+            endGame()
+        }
+        else{
+            displayGuess(guess);
+            ckeckGuess(guess);
+        }
+
+    }
+}
+
+function ckeckGuess(guess) {
+    if (guess === randomNumber) {
+        displayMassage("Your Guess is Right");
+        endGame();
+    } else if(guess < randomNumber) {
+        displayMassage("Your Guess Too low");
+    }
+     else if(guess > randomNumber) {
+        displayMassage("Your Guess Too heigh");
+    }
+
+    
+}
+
+function displayGuess(guess) {
+    userInput.value = '';
+    guessSlot.innerHTML += ` ${guess} `;
+    numGuess++;
+    remaining.innerHTML = `${11 - numGuess}`;
+
+}
+
+function displayMassage(message) {
+    lowOrHi.innerHTML = `<h2>${message}</h2>`;
+    
+}
+
+function endGame() {
+    userInput.value = '';
+    userInput.setAttribute('disabled', '');
+    p.classList.add('button');
+    p.innerHTML = `<h2 id="newGame">Start New Game</h2>`;
+    startOver.appendChild(p);
+    playGame = false;
+    newGame();
+    
+}
+
+function newGame() {
+    const newGameButton = document.querySelector('#newGame');
+    newGameButton.addEventListener('click', function (event) {
+        randomNumber = parseInt(Math.random()*100 +1);
+        prevGuess = [];
+        numGuess = 1;
+        guessSlot.innerHTML = '';
+        remaining.innerHTML = `${11 - numGuess} `;
+        userInput.removeAttribute('disabled');
+        startOver.removeChild(p);
+        playGame = true;
+    })
+}
+
